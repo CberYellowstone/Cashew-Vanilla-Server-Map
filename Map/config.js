b7645205081edf61ffa9d2e70a35f6202f78544c
CBYellowstone <2844829687@qq.com>
2021-03-14 14:48:30 +0800

    Add Map Static

---
+++ Map/config.js
@@ -0,0 +1,153 @@
+var CONFIG = {
+  "maps": {
+    "end_isometric": {
+      "defaultZoom": 8,
+      "imageFormat": "webp",
+      "lastRendered": [
+        1615544073,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 10,
+      "name": "末地",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "end_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "end",
+      "worldName": "end",
+      "worldSeaLevel": 64
+    },
+    "nether_isometric": {
+      "defaultZoom": 12,
+      "imageFormat": "webp",
+      "lastRendered": [
+        1615544073,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 14,
+      "name": "地狱",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "nether_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "nether",
+      "worldName": "nether",
+      "worldSeaLevel": 64
+    },
+    "overworld_isometric_day": {
+      "defaultZoom": 10,
+      "imageFormat": "webp",
+      "lastRendered": [
+        1615544073,
+        0,
+        1615544073,
+        0
+      ],
+      "maxZoom": 14,
+      "name": "主世界",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        2
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "overworld_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "overworld",
+      "worldName": "overworld",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "overworld_isometric_day",
+    "nether_isometric",
+    "end_isometric"
+  ],
+  "tileSetGroups": {
+    "end_isometric_t1": {
+      "maxZoom": 10,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    },
+    "nether_isometric_t1": {
+      "maxZoom": 14,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    },
+    "overworld_isometric_t1": {
+      "maxZoom": 14,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
